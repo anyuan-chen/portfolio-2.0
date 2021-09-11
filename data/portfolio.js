ebb7e629037dd3915a9c1731d0ee87c37ec35ef2
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-11 09:48:20 -0400

    portfolio completion

--- data/portfolio.js
+++ data/portfolio.js
@@ -4,6 +4,11 @@ const portfolio = [
     desc: "Cat Calendar allows you to practice good habits, plan out your day, and much more with a minimalist Pusheen inspired UI. You are followed throughout your day while habits and breaks are randomized.        ",
     img: "/catcalendar.png",
   },
+  {
+      name: "Competitive Programming",
+      desc: "My solutions to competitive programming problems in C++, involving various data structures and algorithms.     ",
+      img: "/competitive.png"
+  }
 ];
 
 module.exports = portfolio;
