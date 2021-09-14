aba14d09106f6da3aa0e76bb97deb2ace7423363
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-13 20:53:02 -0400

    semi-working search-bar

--- components/search.js
+++ components/search.js
@@ -3,6 +3,35 @@ import Post from "./post";
 export default function Search(props) {
   const [curPosts, setCurPosts] = useState(props.posts);
   const [userInput, setUserInput] = useState("");
+  const [postComponents, setPostComponents] = useState(
+    props.posts.map((post) => {
+      return (
+        <Post
+          title={post.frontmatter.title}
+          description={post.frontmatter.description}
+          date={post.frontmatter.date}
+          tags={post.frontmatter.tags}
+          slug={post.slug}
+        ></Post>
+      );
+    })
+  );
+  useEffect(() => {
+    setPostComponents(
+      curPosts.map((post) => {
+        return (
+          <Post
+            title={post.frontmatter.title}
+            description={post.frontmatter.description}
+            date={post.frontmatter.date}
+            tags={post.frontmatter.tags}
+            slug={post.slug}
+          ></Post>
+        );
+      })
+    );
+  }, [curPosts]);
+  
   const changeInput = (event) => {
     setUserInput(event.target.value);
     setCurPosts(
@@ -22,19 +51,7 @@ export default function Search(props) {
         value={userInput}
         onChange={changeInput}
       ></input>
-      <div className="w-page space-y-3 pt-5">
-        {curPosts.map((post) => {
-          return (
-            <Post
-              title={post.frontmatter.title}
-              description={post.frontmatter.description}
-              date={post.frontmatter.date}
-              tags={post.frontmatter.tags}
-              slug={post.slug}
-            ></Post>
-          );
-        })}{" "}
-      </div>
+      <div className="w-page space-y-3 pt-5">{} </div>
     </div>
   );
 }
